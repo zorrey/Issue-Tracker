c640b48514e45f2be60c18f3f17e3e8b618dd6a3
Zorrey <102656748+zorrey@users.noreply.github.com>
2022-07-27 23:52:02 +0300

    database dbConnect

---
+++ dbConnect.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose');
+const database = mongoose.connect(process.env.DB, {useUnifiedTopology : true,
+                                                   useNewUrlParser: true     });
+  module.exports = database;                                                 
